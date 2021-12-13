9ceb96bae4f39b0e0f613299075cda56ee3c6167
ljinming <981010623@qq.com>
2021-12-13 17:29:08 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import App from '@app'
+import App from '@/app';
+import routes from '@/routes';
+// import models from '@/models';
 
-
-// const app = new App({
-//     onSuccess,
-// })
+const app = new App({
+    onSuccess,
+})
 
 // app.setModels(models);
-// app.setRouter(routes);
+ app.setRouter(routes);
 // app.start('#root');
 
 // new Action(app)
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import App from '@/app';
+// import Home from '@/pages/home';
+// import Login from '@/pages/login';
+
+function RouteApp(app: App) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <Route path='/login' exact component={Login} />
+        <Route path='/' component={Home}></Route> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default RouteApp;
